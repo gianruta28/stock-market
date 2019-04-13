1261d959a352e0b8d8fbaeb2caabc527e38f88ed
Gianfranco <gianjs.98@gmail.com>
2019-04-13 16:17:28 +0200

    Page 25 Chapter 2

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockItemComponent implements OnInit {
 
+  public name: string;
+  public code: string;
+  public price: number;
+  public previousPrice: number;
+  public positiveChange: boolean;
+
+
   constructor() { }
 
   ngOnInit() {
+    this.name = 'Test Stock Company';
+    this.code = 'TSC';
+    this.price = 85;
+    this.previousPrice = 80;
+    this.positiveChange = this.price >= this.previousPrice;
   }
 
 }
